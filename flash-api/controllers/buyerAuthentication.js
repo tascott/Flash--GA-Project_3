7d04ad020592ec6c82e0472076bc310dbcf4770e
Paul Crosby <paul71crosby@gmail.com>
2016-06-21 11:38:16 +0100

    buyer token attached

--- flash-api/controllers/buyerAuthentication.js
+++ flash-api/controllers/buyerAuthentication.js
@@ -11,7 +11,7 @@ function login(req, res) {
     var payload = { _id: user._id, username: user.username }; 
     var token = jwt.sign(payload, secret, {expiresIn: 60*60*24});
 
-    return res.status(200).json({ message: "Login successful", user: user , token : token});
+    return res.status(200).json({ message: "Login successful", user: user , buyerToken : token});
   });
 
 }
